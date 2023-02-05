c3522cbe499adccea435db5ee31b6a02cf09faec
Vitalii <itstart.com.ua@gmail.com>
2023-02-05 23:10:48 +0200

    add all fix

---
+++ parts/section-number.php
@@ -0,0 +1,25 @@
+<!-- Секция достижений в цифрах -->
+<section class="section section-number mt-40 mb-0" style="background: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/img/content-number-bg.jpg); background-repeat: no-repeat;">
+    <div class="content-number" >
+    <div class="content-number-title">На сегодняшний день было продано 3632 дипломов</div>
+    <div class="content-number-subtitle">Среди них:</div>
+    <div class="content-number-container">
+        <div class="content-number-item">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/icon-student-1.png" alt="Изготовление дипломов бакалавра на заказ в СПб">
+        <p><span>1512</span>дипломов бакалавра</p>
+        </div>
+        <div class="content-number-item">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/icon-student-2.png" alt="Изготовление аттестатов на заказ в СПб">
+        <p><span>864</span>дипломов магистра</p>
+        </div>
+        <div class="content-number-item">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/icon-student-3.png" alt="Изготовление дипломов магистра на заказ СПб">
+        <p><span>682</span>аттестатов</p>
+        </div>
+        <div class="content-number-item">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/icons/icon-student-4.png" alt="Изготовление подлинных документов на заказ в СПб">
+        <p><span>574</span>других документов</p>
+        </div>
+    </div>
+    </div>
+</section>
